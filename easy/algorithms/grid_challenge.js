f57e1971bb9c2107d8d3d5236088b178b7d4c3d6
Juan Pablo Navarrete <98796242+Juanpnav822@users.noreply.github.com>
2022-07-25 18:09:05 -0400

    Create grid_challenge.js
    Easiest challenge I've made so far.

---
+++ easy/algorithms/grid_challenge.js
@@ -0,0 +1,162 @@
+/*
+https://www.hackerrank.com/challenges/one-week-preparation-kit-grid-challenge/problem
+
+Given a square grid of characters in the range ascii[a-z], rearrange elements of each row alphabetically, ascending. Determine if the columns are also in ascending alphabetical order, top to bottom. Return YES if they are or NO if they are not.
+
+Example
+
+The grid is illustrated below.
+
+a b c
+a d e
+e f g
+The rows are already in alphabetical order. The columns a a e, b d f and c e g are also in alphabetical order, so the answer would be YES. Only elements within the same row can be rearranged. They cannot be moved to a different row.
+
+Function Description
+
+Complete the gridChallenge function in the editor below.
+
+gridChallenge has the following parameter(s):
+
+string grid[n]: an array of strings
+Returns
+
+string: either YES or NO
+Input Format
+
+The first line contains , the number of testcases.
+
+Each of the next  sets of lines are described as follows:
+- The first line contains , the number of rows and columns in the grid.
+- The next  lines contains a string of length 
+
+Constraints
+
+
+
+Each string consists of lowercase letters in the range ascii[a-z]
+
+Output Format
+
+For each test case, on a separate line print YES if it is possible to rearrange the grid alphabetically ascending in both its rows and columns, or NO otherwise.
+
+Sample Input
+
+STDIN   Function
+-----   --------
+1       t = 1
+5       n = 5
+ebacd   grid = ['ebacd', 'fghij', 'olmkn', 'trpqs', 'xywuv']
+fghij
+olmkn
+trpqs
+xywuv
+Sample Output
+
+YES
+Explanation
+
+The x grid in the  test case can be reordered to
+
+abcde
+fghij
+klmno
+pqrst
+uvwxy
+This fulfills the condition since the rows 1, 2, ..., 5 and the columns 1, 2, ..., 5 are all alphabetically sorted.
+*/
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'gridChallenge' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING_ARRAY grid as parameter.
+ */
+
+function gridChallenge(grid) {
+    // Write your code here
+    let abc = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    let arr = [];
+    let temp = 0;
+    let respond = "YES";
+    
+    for (let i=0; i<grid.length; i++){
+      arr[i] = grid[i].split("");
+      
+      for(let j=0; j<grid.length; j++){
+        for(let m=0; m<abc.length; m++){
+          
+          if (arr[i][j]==abc[m]){
+            arr[i][j]=m;
+          }
+        }
+      }
+      
+      for (let n=0; n<grid.length; n++){
+        for (let l=n+1; l<grid.length; l++){
+          
+          if (arr[i][n]>arr[i][l]){
+            temp=arr[i][n];
+            arr[i][n]=arr[i][l];
+            arr[i][l]=temp;
+          }
+        }
+      }
+    }
+    for (let x=0; x < grid.length; x++){
+      for (let y=0; y < grid.length - 1; y++){
+          
+        if (arr[y][x]>arr[y+1][x]){
+          respond = "NO";
+        }
+      }
+    }
+  return respond;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const t = parseInt(readLine().trim(), 10);
+
+    for (let tItr = 0; tItr < t; tItr++) {
+        const n = parseInt(readLine().trim(), 10);
+
+        let grid = [];
+
+        for (let i = 0; i < n; i++) {
+            const gridItem = readLine();
+            grid.push(gridItem);
+        }
+
+        const result = gridChallenge(grid);
+
+        ws.write(result + '\n');
+    }
+
+    ws.end();
+}
