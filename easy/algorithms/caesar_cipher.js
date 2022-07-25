3933f3740c51d6c40f837da50d2740f95d898d5c
Juan Pablo Navarrete <98796242+Juanpnav822@users.noreply.github.com>
2022-07-25 14:33:48 -0400

    Create caesar_cipher.js
    A got infinite loop error while doing this....

---
+++ easy/algorithms/caesar_cipher.js
@@ -0,0 +1,142 @@
+/*
+https://www.hackerrank.com/challenges/one-week-preparation-kit-caesar-cipher-1/problem
+Julius Caesar protected his confidential information by encrypting it using a cipher. Caesar's cipher shifts each letter by a number of letters. If the shift takes you past the end of the alphabet, just rotate back to the front of the alphabet. In the case of a rotation by 3, w, x, y and z would map to z, a, b and c.
+
+Original alphabet:      abcdefghijklmnopqrstuvwxyz
+Alphabet rotated +3:    defghijklmnopqrstuvwxyzabc
+Example
+
+
+The alphabet is rotated by , matching the mapping above. The encrypted string is .
+
+Note: The cipher only encrypts letters; symbols, such as -, remain unencrypted.
+
+Function Description
+
+Complete the caesarCipher function in the editor below.
+
+caesarCipher has the following parameter(s):
+
+string s: cleartext
+int k: the alphabet rotation factor
+Returns
+
+string: the encrypted string
+Input Format
+
+The first line contains the integer, , the length of the unencrypted string.
+The second line contains the unencrypted string, .
+The third line contains , the number of letters to rotate the alphabet by.
+
+Constraints
+
+
+
+ is a valid ASCII string without any spaces.
+
+Sample Input
+
+11
+middle-Outz
+2
+Sample Output
+
+okffng-Qwvb
+Explanation
+
+Original alphabet:      abcdefghijklmnopqrstuvwxyz
+Alphabet rotated +2:    cdefghijklmnopqrstuvwxyzab
+
+m -> o
+i -> k
+d -> f
+d -> f
+l -> n
+e -> g
+-    -
+O -> Q
+u -> w
+t -> v
+z -> b
+*/
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'caesarCipher' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. STRING s
+ *  2. INTEGER k
+ */
+
+function caesarCipher(s, k) {
+    // Write your code here
+  k=k%26;
+  let abc = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  let upper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+  const myArray = s.split('');
+  let r = [];
+  
+  for(let i=0; i<myArray.length; i++){
+    for(let j=0; j<abc.length; j++){
+      
+      if(myArray[i]==abc[j] && j+k<26){
+        r[i]=abc[j+k];
+      }
+      if(myArray[i]==upper[j] && j+k<26){
+        r[i]=upper[j+k]
+      }
+      if(myArray[i]==abc[j] && j+k>25){
+        r[i]=abc[j+k-26]
+      }
+      if(myArray[i]==upper[j] && j+k>25){
+        r[i]=upper[j+k-26]
+      }
+    } 
+    if(r[i]==null){
+      r[i]=myArray[i];
+    }
+  }
+let aja = r.join('');
+  
+  return aja;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const s = readLine();
+
+    const k = parseInt(readLine().trim(), 10);
+
+    const result = caesarCipher(s, k);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
